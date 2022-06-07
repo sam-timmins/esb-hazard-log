6f61f39ec7c35a7f110286cef70bbd2c7e92bb1d
Sam Timmins <sam.timmins1@gmail.com>
2022-06-07 22:18:14 +0100

    Add location view

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def location(request):
+    """ A view to return the index page """
+
+    return render(request, 'locations/location.html')
